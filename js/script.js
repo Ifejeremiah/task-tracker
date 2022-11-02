13a05c41da04498951936b5a81acdcd90883258f
Ife Jeremiah <jerryroboy.16@gmail.com>
2022-11-02 13:25:06 +0100

    Add handler to display message when no task

--- js/script.js
+++ js/script.js
@@ -1,63 +1,64 @@
-const formContainer = document.querySelector(".create-task")
-const taskContainer = document.querySelector(".con-task")
-const headerBtn = document.getElementById("header-btn")
-const form = document.getElementById("add-task")
+const formContainer = document.querySelector(".create-task");
+const taskContainer = document.querySelector(".con-task");
+const headerBtn = document.getElementById("header-btn");
+const form = document.getElementById("add-task");
 
-const taskField = form.querySelector('#task')
-const dateField = form.querySelector('#date')
+const taskField = form.querySelector("#task");
+const dateField = form.querySelector("#date");
 
-let formActive
+let formActive;
 
+headerBtn.addEventListener("click", toggleForm);
 
-headerBtn.addEventListener('click', toggleForm)
+form.addEventListener("submit", handleForm);
 
-form.addEventListener('submit', handleForm)
-
-handleDelete()
+handleDelete();
 
 function toggleForm() {
-  formContainer.classList.toggle('active')
-  if (!formActive) formActive = true
-  else formActive = false
-  handleBtn()
+  formContainer.classList.toggle("active");
+  if (!formActive) formActive = true;
+  else formActive = false;
+  handleBtn();
 }
 
 function handleBtn() {
   if (formActive) {
-    headerBtn.classList.add('active')
-    headerBtn.innerText = 'Close'
+    headerBtn.classList.add("active");
+    headerBtn.innerText = "Close";
   } else {
-    headerBtn.classList.remove('active')
-    headerBtn.innerText = 'Add'
+    headerBtn.classList.remove("active");
+    headerBtn.innerText = "Add";
   }
 }
 
 function clearInput() {
-  taskField.value = ''
-  dateField.value = ''
+  taskField.value = "";
+  dateField.value = "";
 
-  taskField.blur()
-  dateField.blur()
+  taskField.blur();
+  dateField.blur();
 }
 
 function createTask() {
-  const task = document.createElement('div')
-  task.classList.add('task')
-  task.innerHTML =
-    template(taskField.value, new Date(dateField.value).toDateString())
+  const task = document.createElement("div");
+  task.classList.add("task");
+  task.innerHTML = template(
+    taskField.value,
+    new Date(dateField.value).toDateString()
+  );
 
-  taskContainer.prepend(task)
+  taskContainer.prepend(task);
 }
 
 function handleForm(evt) {
-  evt.preventDefault()
+  evt.preventDefault();
 
-  if (!taskField.value || !dateField.value)
-    return
+  if (!taskField.value || !dateField.value) return;
 
-  createTask()
-  clearInput()
-  handleDelete()
+  clearMsg();
+  createTask();
+  clearInput();
+  handleDelete();
 }
 
 function template(title, date) {
@@ -69,12 +70,30 @@ function template(title, date) {
     <div class="delete pointer" id="delete-icon">
       <i class="fa-regular fa-trash-can"></i>
     </div>
-  `
+  `;
 }
 
 function handleDelete() {
-  const deleteBtn = document.getElementById('delete-icon')
-  const task = document.querySelector('.task')
+  const deleteBtn = document.getElementById("delete-icon");
+  const task = document.querySelector(".task");
 
-  deleteBtn.addEventListener('click', () => task.remove())
+  deleteBtn.addEventListener("click", () => {
+    task.remove();
+    sendMsg();
+  });
+}
+
+function sendMsg() {
+  const tasks = taskContainer.querySelectorAll(".task");
+  if (!tasks.length > 0) {
+    const msg = document.createElement("div");
+    msg.classList.add("msg");
+    msg.innerText = "No tasks available";
+    taskContainer.append(msg);
+  }
+}
+
+function clearMsg() {
+  const msg = taskContainer.querySelector(".msg");
+  msg.remove();
 }
